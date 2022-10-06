c07c9c96cc6b961dc7688b34f40007ccd8286617
Richard Cruz <ricktonycr@gmail.com>
2022-10-05 23:25:41 -0500

    adding a module cofig

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ricktonycr/gx_parser
+
+go 1.19
+
+require github.com/antlr/antlr4/runtime/Go/antlr v1.4.10
+
+require (
+	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20220911224424-aa1f1f12a846 // indirect
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+)
--- gxparser_listener.go
+++ gxparser_listener.go
@@ -1,8 +1,8 @@
-// Code generated from GXParser.g4 by ANTLR 4.9. DO NOT EDIT.
+// Code generated from java-escape by ANTLR 4.11.1. DO NOT EDIT.
 
 package parser // GXParser
 
-import "github.com/antlr/antlr4/runtime/Go/antlr"
+import "github.com/antlr/antlr4/runtime/Go/antlr/v4"
 
 // GXParserListener is a complete listener for a parse tree produced by GXParser.
 type GXParserListener interface {
